0d3dce3f743ba02e8c113bfb7beb9cbc1d3daca6
Dorazar <dorazar01@gmail.com>
2025-04-02 14:18:26 +0300

    Add save and load from local storage

--- userService.js
+++ userService.js
@@ -3,11 +3,19 @@ const user = {
   txtColor: '',
 }
 
+
+
+
 function onSubmit(ev) {
   ev.preventDefault()
   const elEmail = document.querySelector('[name=email]').value
   const elTextColor = document.querySelector('[name=txtColor]').value
 
-  console.log(elEmail)
-  console.log(elTextColor)
+  user.email = elEmail
+  user.txtColor = elTextColor
+  //   console.log(user)
+  saveToLocalStorage('userData', user)
+  var res = loadFromLocalStorage('userData')
+  console.log(res)
 }
+
---
+++ utilService.js
@@ -0,0 +1,9 @@
+function saveToLocalStorage(key, value) {
+  const json = JSON.stringify(value)
+  localStorage.setItem(key, json)
+}
+
+function loadFromLocalStorage(key) {
+  const json = JSON.parse(localStorage.getItem(key))
+  return json
+}
